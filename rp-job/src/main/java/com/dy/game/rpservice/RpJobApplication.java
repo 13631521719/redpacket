68ede3452bb825ef92244556c1bf8975ee370aaf
13631521719 <598601307@qq.com>
2021-02-28 20:39:40 +0800

    初始化

---
+++ rp-job/src/main/java/com/dy/game/rpservice/RpJobApplication.java
@@ -0,0 +1,13 @@
+package com.dy.game.rpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpJobApplication.class, args);
+    }
+
+}
---
+++ rp-service/src/main/java/com/dy/game/rpservice/RpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dy.game.rpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpServiceApplication.class, args);
+    }
+
+}
